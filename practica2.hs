0c68a11a0e8ca1f2f60355022eb4eaf7d31be1c4
soffochoa <soffochoa@ciencias.unam.mx>
2024-09-20 13:41:57 -0600

    Práctica actualizada

--- practica2.hs
+++ practica2.hs
@@ -1,3 +1,4 @@
+import Distribution.Types.Lens (PackageIdentifier)
 --PRACTICA LÓGICA PROPOSICIONAL.
 
 -- Definición del tipo LProp
@@ -16,3 +17,25 @@ vars (Conj p q) = vars p ++ vars q
 vars (Disy p q) = vars p ++ vars q
 vars (Impl p q) = vars p ++ vars q
 vars (Syss p q) = vars p ++ vars q
+
+--2. deMorgan: Toma una f´ormula proposicional y regresa su valor aplicando las leyes de De Morgan. Por ejemplo: deMorgan ¬(p ∧ q) regresa (¬p ∨ ¬q).
+--Si nose aplica la ley, regresa la f´ormula original. 
+deMorgan :: LProp -> LProp
+deMorgan PTrue = PTrue
+deMorgan PFalse = PFalse
+deMorgan (Var n) = (Var n)
+deMorgan (Neg n) = Neg (deMorgan n)
+--deMorgan (Conj a b) = Conj (deMorgan a) (deMorgan b)
+deMorgan (Neg (Conj a b)) = Disy (Neg (deMorgan a)) (Neg (deMorgan b))
+--deMorgan (Disy a b) = Disy (deMorgan a) (deMorgan b)
+deMorgan (Neg (Disy a b)) = Conj (Neg (deMorgan a)) (Neg (deMorgan b))
+deMorgan (Impl a b) = Impl (deMorgan a) (deMorgan b)
+deMorgan (Syss a b) = Syss (deMorgan a) (deMorgan b)
+
+--3. equiv op: Dada una formula con implicacion → regresa su equivalente con conectores basicos. Por ejemplo, para p → q regresara el valor ¬p ∨ q.
+--4.dobleNeg: Dada una formula con doble negacion elimina esta. Por ejemplo, para ¬(¬p) regresar p.
+--5. num conectivos: Funcion recursiva para contar el n´umero de conectivos logicos de una formula
+--6. num variables: Funcion recursiva para contar el n´umero de variables de una formula.
+--7. profundidad: Funcion recursiva que regresa la profundidad de una f´ormula l´ogica.
+--8.interpretacion: Regresa los valores de verdad, True o False, segun una asig-naci´on. Toma como argumentos una f´ormula y una asignaci´on de las variables.
+--Por ejemplo: interpretacion (p∧q) [(”p”, True), (”q”, False)] regresar´a True.
